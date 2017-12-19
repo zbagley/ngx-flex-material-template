ceec9281c2ea16c4e28dc0a44e63287f4a867f33
Zachary T. Bagley <zbagley@datastandards.limited>
2017-12-19 08:29:49 -0700

    update routing

--- src/app/routing.module.ts
+++ src/app/routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { AppComponent } from './app.component';
 import { TopNavComponent } from './top-nav/top-nav.component';
 import { FooterComponent } from './footer/footer.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
@@ -11,6 +10,7 @@ export class EmptyComponent {}
 
 const routes: Routes = [
   { path: 'acct', loadChildren: './acct/acct.module#AcctModule' },
+  { path: 'option1', loadChildren: './home/home.module#HomeModule' },
   { path: 'option2', loadChildren: './option2/option2.module#Option2Module' },
   { path: 'option3', loadChildren: './option3/option3.module#Option3Module' },
   { path: '404', component: PageNotFoundComponent },
